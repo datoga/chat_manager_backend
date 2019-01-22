47a6000854467c0dac92c5d594307a38f11456dd
unknown <dtorres@fermax.com>
2019-01-22 08:28:53 +0100

    First connection to mongoDB

--- cmd/chatmanager/chatmanager.go
+++ cmd/chatmanager/chatmanager.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/datoga/chat_manager_backend/db"
+
+	"github.com/datoga/chat_manager_backend/service"
+)
 
 func main() {
-	fmt.Println("Hola")
+
+	connstr := "mongodb://chatmanager:1123581321chatmanager@ds159624.mlab.com:59624/chatmanager"
+
+	dbchatmanager := db.NewMongoAccessor(connstr)
+
+	log.Println("Connecting mongo")
+
+	err := dbchatmanager.Connect()
+
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	chatmanager := service.NewChatManagerService(dbchatmanager)
+
+	chat, err := chatmanager.NewChat("prueba", "esta es una prueba")
+
+	if err != nil {
+		log.Println(err)
+		os.Exit(2)
+	}
+
+	log.Println(chat)
+
+	log.Println("Finish program")
 }
